7652fc4a1b3355cdf1f1ed55be3ea4a7890b3722
Onam Chilwan <onamch@gmail.com>
2016-03-30 16:39:35 +0100

    Adding project

---
+++ src/Fibonacci.Client/FibonacciGenerator.cs
@@ -0,0 +1,29 @@
+﻿namespace Fibonacci.Client
+{
+    using System.Collections.Generic;
+
+    public class FibonacciGenerator
+    {
+        public IEnumerable<int> Generate(int numberOfSteps)
+        {
+            var numbers = new List<int>();
+
+            for (var index = 0; index < numberOfSteps; index++)
+            {
+                if (index > 1)
+                {
+                    var firstDigit = numbers[index - 2];
+                    var secondDigit = numbers[index - 1];
+                    var sum = firstDigit + secondDigit;
+
+                    numbers.Add(sum);
+                    continue;
+                }
+
+                numbers.Add(index);
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ src/Fibonacci.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Fibonacci.Client
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacci = new FibonacciGenerator();
+
+            Console.WriteLine("Enter an invalid number to quit.");
+
+            while (true)
+            {
+                Console.WriteLine("Enter the number of sequences: ");
+                int steps;
+
+                if (int.TryParse(Console.ReadLine(), out steps))
+                {
+                    var numbers = fibonacci.Generate(steps).ToList();
+
+                    Console.WriteLine("-------------------");
+
+                    numbers.ForEach(Console.WriteLine);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
